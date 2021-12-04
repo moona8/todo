e7282e5dc482554942cb518efa69ab71c72a49ac
moona8 <73126379+moona8@users.noreply.github.com>
2021-12-04 21:13:14 +0500

    todo app

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [value, setValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handelChange = (value) => {
+    setValue(value);
+  };
+  const onAddTodo = () => {
+    setTodos([...todos, value]);
+    setValue("");
+  };
+  const onDellTodo = (i) => {
+    const Todos = [...todos];
+    Todos.splice(i, 1);
+    setTodos(Todos);
+  };
+  const onDellAll=()=>{
+    setTodos([])
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Todo App</Text>
+      <TextInput
+        value={value}
+        onChangeText={handelChange}
+        placeholder="Enter  Todo Item"
+      />
+      <Button onPress={onAddTodo} title="Add Todo"></Button>
+      {todos.map((todo, i) => (
+        <Text>
+          {todo}
+          <Button
+            onPress={() => onDellTodo(i)}
+            title="Delete"
+            color="#ff0000"
+          />
+        </Text>
+      ))}
+      <Button onPress={onDellAll} title="Delete All"></Button>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +50,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
